0232dd00154dded068c36e18111b318bfab7c313
Juvina55 <89817966+Juvina55@users.noreply.github.com>
2021-10-15 22:15:31 +0800

    Add files via upload

---
+++ Experiment5/src/Chef.java
@@ -0,0 +1,12 @@
+public class Chef {
+ 
+	public void muttonstring() {
+		System.out.println("Chef barbecue a mutton string");
+	}
+	 
+	public void chicken() {
+		System.out.println("Chef barbecue a chicken");
+	}
+	 
+}
+ 
